2f438e8703ffd414a5dffc1c5a77ae9ea45b97bf
Batyrq14 <122871602+Batyrq14@users.noreply.github.com>
2023-10-14 12:54:24 +0600

    added files

---
+++ Labs/Lab1/Problem1/Analyzer.java
@@ -0,0 +1,19 @@
+package Lab1;
+
+import java.util.Scanner;
+
+public class Analyzer {
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+        
+        
+
+        while(true){
+            System.out.println("Enter number (Q to quit):");
+            String dat = scan.next();
+            if(dat  )
+
+        }
+    }
+    
+}
---
+++ Labs/Lab1/Problem1/Data.java
@@ -0,0 +1,15 @@
+package Lab1;
+
+public class Data{
+    private double d;
+    private double d1;
+    private int num;
+
+
+    public Data(double d, double d1, int num){
+        this.d = d;
+        this.d1 = d1;
+        this.num = num;
+    }
+    
+}
